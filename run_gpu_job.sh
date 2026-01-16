36925a5c4cdd0e028b5bab15399c89d3bb110562
Christian Ispas <ispas@vera2.int.private>
2026-01-16 18:28:09 +0100

    git ignore config on cupy branch

---
+++ run_gpu_job.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=lbm_gpu
+#SBATCH --account=C3SE2025-2-17   # <— your project
+#SBATCH --partition=vera          # use the Vera partition
+#SBATCH --gpus-per-node=A40:1     # request one A40 GPU
+#SBATCH --nodes=1
+#SBATCH --time=04:00:00           # wall-time (4 hours)
+#SBATCH --output=lbm_%j.out       # job output log
+
+
+module purge
+module load foss/2025b
+module load CUDA/12.9.1
+module load CuPy/13.6.0-foss-2025a
+source venv/bin/activate
+
+python3 samples/isentropic_vortex.py
